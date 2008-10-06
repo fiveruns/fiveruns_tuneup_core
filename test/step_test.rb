ba13037beed9253c02a9508875a06b4c0c1aadc4
Bruce Williams <bruce@codefluency.com>
2008-10-06 11:42:16 -0500

    Add Step.load testing

---
+++ test/step_test.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+
+require 'rubygems'
+require 'shoulda'
+
+require File.dirname(__FILE__) << "/../lib/fiveruns_tuneup_core"
+
+class StepTest < Test::Unit::TestCase
+  
+  context "loading from JSON" do
+    setup do
+      @json = read_json(:a)
+      @root = Fiveruns::Tuneup::Step.load(@json)
+    end
+    should "create a RootStep" do
+      assert_kind_of Fiveruns::Tuneup::RootStep, @root
+      assert_equal JSON.load(@json)['children'].size, @root.children.size
+    end
+    should "have normal children" do
+      @root.children.each do |child|
+        assert_normal_node(child)
+      end
+    end
+  end
+  
+  def assert_normal_node(node)
+    assert node.is_a?(Fiveruns::Tuneup::Step)
+    node.children.each do |child|
+      assert_normal_node(child)
+    end
+  end
+  
+  def read_json(name)
+    File.read(File.dirname(__FILE__) << "/fixtures/#{name}.json")
+  end
+    
+  
+end
